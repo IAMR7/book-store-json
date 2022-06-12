c1c2f36d957bdf9c9d0ad4716344b59ab6c4a6db
IAMR7 <reza.febri72@gmail.com>
2022-06-12 16:39:05 +0700

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,16 @@
-$.getJSON("../assets/js/buku.json", function(data){
-    // console.log(data);
+// $.getJSON("../assets/js/buku.json", function(data){
 
-    $.each(data.buku, function(i, data){
-        $('#list-buku').append('<div class="col-lg-3 col-md-4 col-6 mb-5"><div class="card"><img src="' + data.img + '" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title fw-bolder">'+ data.judul +'</h5><p class="card-text">'+ data.description +'</p><a href="#" class="btn text-white btn-buy w-100"> <i class="bi bi-cart2"></i> ' + data.harga + '<a></div></div></div>');
-    });
+//     $.each(data.buku, function(i, data){
+//         $('#list-buku').append('');
+//     });
     
+
+// });
+
+$.getJSON("../assets/js/buku.json", function(data){
+    
+    $.each(data.buku, function(i, data){
+        $('#list-buku').append('<div class="col-lg-3 col-md-4 col-6"><div class="card" style="width: 18rem;"><img src="'+ data.img +'" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">'+ data.judul +'</h5><p class="card-text">'+ data.categorie +'</p><a href="#" class="btn btn-primary">'+ data.harga +'</a></div></div></div>');
+    });
 
 });
